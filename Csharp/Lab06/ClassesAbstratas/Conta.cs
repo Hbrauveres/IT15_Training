d3ac4979d02e3cd6d50827c02347d070600967ab
hbrauveres <henrique.brauveres@gmail.com>
2022-05-23 13:34:47 -0300

    Lab06 Concluido

---
+++ Csharp/Lab06/ClassesAbstratas/Conta.cs
@@ -0,0 +1,40 @@
+public abstract class Conta
+{
+    private decimal saldo;
+    private string titular;
+
+    public Conta(string t)
+    {
+        titular = t;
+    }
+
+    public decimal Saldo 
+    {
+        get{ return saldo; }
+    }
+
+    public string Titular
+    {
+        get { return titular; }
+    }
+
+    public abstract string Id
+    {
+        get;
+    }
+
+    public virtual void Depositar(decimal valor)
+    {
+        saldo += valor;
+    }
+
+    public virtual void Sacar(decimal valor)
+    {
+        saldo -= valor;
+    }
+
+    public virtual string Extrato()
+    {
+        return $"TITULAR: {this.Titular}\nSALDO: {this.Saldo}\n";        
+    }
+}
---
+++ Csharp/Lab06/ClassesAbstratas/Program.cs
@@ -0,0 +1,5 @@
+﻿ContaPoupanca c1 = new ContaPoupanca(0.5m, DateTime.Now, "Henrique Silva");
+Console.WriteLine(c1.Saldo);
+c1.Depositar(100);
+c1.Sacar(103);
+Console.WriteLine(c1.Extrato());
