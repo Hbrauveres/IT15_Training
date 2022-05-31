359c726c58519c334ed2a09715a45089a5b6e39f
hbrauveres <henrique.brauveres@gmail.com>
2022-05-31 13:33:02 -0300

    [ASPNET] MVC Intro usando MVC with razor views

---
+++ ASPNET/MVCIntroducao/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVCIntroducao.Models;
+
+namespace MVCIntroducao.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public string AloMundo(string name) // .../Home/AloMundo?name=Henrique
+    {
+        return $"Alo, {name}"; // FURO DE SEGURANÇA!!!!!!!!!
+                               // Recebendo valor nao verificado para execução
+    }
+
+    public IActionResult Index() // .../Home/Index ou /
+    {
+        return View();
+    }
+
+    public IActionResult Privacy() // .../Home/Privacy
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() // .../Home/Error
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
